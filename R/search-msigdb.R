6b17f1613076e86ca242d7f923e38e04277cf514
Sean Corbett <scorbett@bu.edu>
2016-07-31 17:53:21 -0400

    Clarifying that the MSigDB gene set searching function works by *matching,* and doesn't need the whole gene set name.

--- R/search-msigdb.R
+++ R/search-msigdb.R
@@ -3,7 +3,7 @@
 #' This is a convenience function to look through the MSigDB gene set collections (C1-C7 and Hallmark) for a gene set
 #' with a known name, and to return the Entrez gene IDs associated with that gene set. Pretty simple!
 #' 
-#' @param gene.set.name The name of the gene set for which to find gene IDs
+#' @param gene.set.name The name (in whole, or part!) of the gene set for which to find gene IDs
 #' @keywords msigdb
 #' @return The MSigDB gene set's Entrez IDs, as a character vector. Or, a list of gene sets, if there were multiple matches. The leading part of each result gene set (the portion before the '.') indicates which gene set collection the gene set was found in.
 #' @export
